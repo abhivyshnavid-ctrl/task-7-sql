553ba62c006c925d75db0388b921e9e61e9ffc37
Abhi vyshnavi <abhivyshnavid@gmail.com>
2026-06-09 11:03:37 +0530

    Add files via upload

---
+++ task 7 sql.sql
@@ -0,0 +1,53 @@
+  -- show databases;
+ -- create database customerdb;
+ -- Use customerdb;
+-- CREATE TABLE Customers (CustomerID INT PRIMARY KEY,CustomerName VARCHAR(50));
+-- CREATE TABLE Orders (OrderID INT PRIMARY KEY,CustomerID INT);
+-- INSERT INTO Customers VALUES(1, 'ravi'),(2, 'sitha'),(3, 'rahul');
+-- INSERT INTO Orders VALUES(101, 1),(102, 2);
+-- select * from customers;
+-- select * from Orders;
+-- UPDATE Customers
+-- SET CustomerName = 'Abhi'
+-- WHERE CustomerID = 1;
+-- UPDATE Customers
+-- SET CustomerName = 'Hasini'
+-- WHERE CustomerID = 2;
+-- UPDATE Customers
+-- SET CustomerName = 'Prasanna'
+-- WHERE CustomerID = 3; 
+-- innerjoin
+-- SELECT C.CustomerID, C.CustomerName, O.OrderID
+-- FROM Customers C
+-- INNER JOIN Orders O
+-- ON C.CustomerID = O.CustomerID; 
+-- left join
+-- SELECT C.CustomerID, C.CustomerName, O.OrderID
+-- FROM Customers C
+-- LEFT JOIN Orders O
+-- ON C.CustomerID = O.CustomerID;
+-- insert(103,4)
+-- INSERT INTO Orders VALUES (103, 4);
+-- SELECT O.OrderID, O.CustomerID
+-- FROM Orders O
+-- LEFT JOIN Customers C
+-- ON O.CustomerID = C.CustomerID
+-- WHERE C.CustomerID IS NULL;
+-- full outer join 
+-- SELECT C.CustomerID, C.CustomerName, O.OrderID
+-- FROM Customers C
+-- LEFT JOIN Orders O
+-- ON C.CustomerID = O.CustomerID
+-- UNION
+-- SELECT C.CustomerID, C.CustomerName, O.OrderID
+-- FROM Customers C
+-- RIGHT JOIN Orders O
+-- ON C.CustomerID = O.CustomerID;
+-- INSERT INTO Orders VALUES (104, 1);
+-- SELECT C.CustomerID,
+       -- C.CustomerName,
+       -- COUNT(O.OrderID) AS TotalOrders
+-- FROM Customers C
+-- LEFT JOIN Orders O
+-- ON C.CustomerID = O.CustomerID
+-- GROUP BY C.CustomerID, C.CustomerName;
